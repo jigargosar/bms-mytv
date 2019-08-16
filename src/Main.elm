3fe766367eba3198601df1b7625b4ab78c3c4973
Jigar Gosar <jigar.gosar@gmail.com>
2019-08-16 18:32:07 +0530

    demo video

--- src/Main.elm
+++ src/Main.elm
@@ -8,8 +8,10 @@ import FontAwesome.Attributes
 import FontAwesome.Icon as FAIcon
 import FontAwesome.Styles
 import HasErrors
-import Html.Styled as H exposing (Html, div, text)
-import Html.Styled.Attributes exposing (class, href)
+import Html
+import Html.Attributes as HA
+import Html.Styled as H exposing (Html, div, source, text, video)
+import Html.Styled.Attributes as A exposing (class, href, src, type_)
 import Html.Styled.Events exposing (onClick)
 import Html.Styled.Lazy exposing (lazy)
 import Http
@@ -98,8 +100,23 @@ init encodedFlags url key =
         |> command fetchData
 
 
+fetchData_ =
+    Http.request
+        { method = "GET"
+        , url = "https://cors-anywhere.herokuapp.com/https://in.bookmyshow.com/serv/getData?cmd=GETVIDEOS&category=MYTV"
+        , expect = Http.expectJson GotData JD.value
+        , headers = []
+        , body = Http.emptyBody
+        , timeout = Nothing
+        , tracker = Nothing
+        }
+
+
 fetchData =
-    Http.get { url = "", expect = Http.expectJson GotData JD.value }
+    Http.get
+        { url = "https://cors-anywhere.herokuapp.com/https://in.bookmyshow.com/serv/getData?cmd=GETVIDEOS&category=MYTV"
+        , expect = Http.expectJson GotData JD.value
+        }
 
 
 
@@ -230,9 +247,26 @@ viewRoot : Model -> StyledDocument Msg
 viewRoot model =
     { title = "Movie Trailers"
     , body =
-        [ div [] [ text "hw" ]
+        [ div []
+            [ text "hw"
+            ]
+        , div [ class "pa3 dn" ] [ lazy lv 1 ]
         ]
     }
+
+
+lv : a -> Html msg
+lv _ =
+    video
+        [ A.id "vid1"
+        , class "azuremediaplayer amp-default-skin"
+        ]
+        [ source
+            [ src "//bmsmedia.streaming.mediaservices.windows.net/9e2d5489-82dd-42cc-8cbb-6726a9f34cfa/roommates agreement revised.ism/manifest(format=m3u8-aapl)"
+            , type_ "application/vnd.ms-sstr+xml"
+            ]
+            []
+        ]
 
 
 faBtn : msg -> FAIcon.Icon -> Html msg
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import 'tachyons'
 import './index.css'
 import { Elm } from './Main.elm'
 import { forEachObjIndexed, isNil, mapObjIndexed, path, propOr } from 'ramda'
@@ -59,3 +60,25 @@ function initPubs(pubs) {
     }
   })(pubs)
 }
+
+
+var myPlayer = amp('vid1', { /* Options */
+    techOrder: ["azureHtml5JS", "flashSS", "html5FairPlayHLS","silverlightSS", "html5"],
+    "nativeControlsForTouch": false,
+    autoplay: false,
+    controls: true,
+    width: "640",
+    height: "400",
+    poster: ""
+  }, function() {
+    console.log('Good to go!');
+    // add an event listener
+    this.addEventListener('ended', function() {
+      console.log('Finished!');
+    })
+  }
+);
+// myPlayer.src([{
+//   src: "http://samplescdn.origin.mediaservices.windows.net/e0e820ec-f6a2-4ea2-afe3-1eed4e06ab2c/AzureMediaServices_Overview.ism/manifest",
+//   type: "application/vnd.ms-sstr+xml"
+// }]);

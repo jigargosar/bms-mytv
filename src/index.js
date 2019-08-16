0047cdc5143a83dcd39606018116daa2b1f8a5ea
Jigar Gosar <jigar.gosar@gmail.com>
2019-08-16 17:06:48 +0530

    init

---
+++ src/index.js
@@ -0,0 +1,70 @@
+import './index.css'
+import { Elm } from './Main.elm'
+// import { Elm } from './elm.min'
+import { Fire } from './fire'
+import {
+  mapObjIndexed,
+  identity,
+  propOr,
+  forEachObjIndexed,
+  path,
+  isNil,
+} from 'ramda'
+
+const storageKey = 'elm-bms-movie-trailers-cache'
+const app = Elm.Main.init({
+  flags: {
+    cache: JSON.parse(localStorage.getItem(storageKey) || 'null'),
+  },
+})
+
+const pubs = initPubs({})
+
+initSubs({
+  localStorageSetJsonItem: ([k, v]) => {
+    console.groupCollapsed('localStorageSetJsonItem', k)
+    console.log(v)
+    console.groupEnd()
+    localStorage.setItem(k, JSON.stringify(v))
+  },
+  setCache: cache => {
+    if (isNil(cache)) {
+      localStorage.removeItem(storageKey)
+    } else {
+      localStorage.setItem(storageKey, JSON.stringify(cache))
+    }
+  },
+})
+
+function initSubs(subs) {
+  forEachObjIndexed((listener, portName) => {
+    const subscribe = path(['ports', portName, 'subscribe'])(app)
+    if (!subscribe) {
+      console.warn('Subscribe: Port Not Found:', portName)
+      return
+    }
+    console.debug('Subscribe: Port Handler Attached', portName)
+    subscribe(listener)
+  })(subs)
+  const ports = propOr({}, 'ports')(app)
+  forEachObjIndexed((port, portName) => {
+    if (port.subscribe && !subs[portName]) {
+      console.warn('Subscribe: Port Handler Missing', portName)
+    }
+  })(ports)
+}
+
+function initPubs(pubs) {
+  return mapObjIndexed((fn, portName) => {
+    return arg => {
+      const send = path(['ports', portName, 'send'])(app)
+      if (!send) {
+        console.warn('Send: Port Not Found:', portName, arg)
+        return
+      }
+      if (send) {
+        send(arg)
+      }
+    }
+  })(pubs)
+}
